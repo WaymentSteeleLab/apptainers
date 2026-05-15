40c7b506a5b8008ed71af223f0fffdc8e3a211d8
Hannah Wayment-Steele <hannah.wayment.steele@gmail.com>
2026-05-15 15:37:27 -0500

    Refactor run.sh to use PDB file and base name

--- dyna1/run.sh
+++ dyna1/run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-PDB=$1
+PDB_FILE=$1
+PDB_BASE=$(basename $PDB_FILE .pdb)
 
 apptainer run \
     --env HF_TOKEN=<YOUR_HF_TOKEN> \
-    /staging/$(USER)/dyna1.sif \
+    /staging/waymentsteel/dyna1.sif \
     /opt/dyna1/dyna1.py \
-    --pdb $PDB \
+    --pdb $PWD/$PDB_FILE \
     --chain A \
-    --name $PDB \
+    --name $PDB_BASE \
     --use_pdb_seq \
     --write_to_pdb \
     --save_dir $PWD
